1c1eec0c6cd838e27c71414b18d1647a9519c339
LoSSeth <aph095ba08@gmail.com>
2021-01-07 13:37:20 +0100

    Major documentation overhaul
    - Add docstrings to Bot class and Bot class methods
    - Add comments
    - Add typing
    - Remove useless method

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from logging import log
 import os
 
 import discord
@@ -6,58 +7,83 @@ from discord.ext import commands
 from PIL import Image
 import requests
 
-class Cbot():
+class Bot:
+  """
+  A discord bot.
+  
+  It logs messages by default and its default log path is 'log.txt'
+  
+  Optional attributes:
+    log: Check wether logging is enabled
+    log_file: Log output file
+  """
+  def __init__(self, log: bool=True, log_file: str='log.txt'):
+    self._log = log
+    self._log_file = log_file
 
-  def __init__(self, log=True, log_file='log.txt'):
-    self.log = log
-    self.message = 'Default message!'
-
-  def clog(self, user, content, channel, time):
-    if self.log:
-      self.write_log(f'{user} wrote \'{content}\' in {channel} at {time}\n')
+  def clog(self, 
+           user: discord.member.Member, 
+           content: str, channel: discord.channel.TextChannel, 
+           time: discord.channel.TextChannel
+           ) -> None:
+    """
+    Logs message, returns None
+    
+    Parameters:
+      user: User who wrote the message
+      content: Body of the message
+      channel: Channel the message has been sent in
+      time: The time at which the message has been sent
+    """
+    _message = f'{user} wrote \'{content}\' in {channel} at {time}\n'
+    if self._log:
+      with open(self._log_file, 'a+') as file:
+        # Format message into utf-8 as unicode does not show properly in file
+        file.write(_message.encode('ascii', 'ignore').decode('utf-8'))
+    return None
         
-  def enable_logging(self):
-    if self.log:
+  def enable_logging(self) -> str:
+    """Toggles logging on, return string with log status"""
+    if self._log:
       _string = 'Logging is already enabled!'
     else:
-      self.log = True
+      self._log = True
       _string = 'Logging is now enabled!'
       self.write_log('##### Logging is enabled #####\n')
     return _string
 
-  def disable_logging(self):
-    if not self.log:
+  def disable_logging(self) -> str:
+    """Toggles logging off, return string with log status"""
+    if not self._log:
       _string = 'logging is already disabled!' 
     else:
-      self.log = False
+      self._log = False
       _string = 'Logging is now disabled!'
       self.write_log('##### Logging disabled #####\n')
     return _string
 
-  def shift_image(self, image_url, shift=20):
+  def shift_image(self, image_url: str, shift: int=20) -> discord.File:
+    """
+    Shifts the value of every pixel in image by set amount
+    
+    Parameters:
+      image_url: URL leading to image
+      
+    Optional parameters:
+      shift: Amount every pizel in image should be changed by. Should be value bteween -255 and 255
+    """
     img = Image.open(self.get_image(image_url))
     pixels = img.load()
     for _col in range(img.size[1]):
       for _row in range(img.size[0]):
         _pixel = [x + shift -255 if x > 255 - shift else x + shift for x in list(pixels[_row,_col])]
         pixels[_row,_col] = tuple(_pixel)
-
     img.save('img_new.png')
     return discord.File('img_new.png', filename='img_new.png')
 
-  def encrypt_message(self, message, key):
-    for char in list(chr(message)):
-      pass
-            
-
   @staticmethod
-  def write_log(message, file='log.txt'):
-    with open(file, 'a+') as _file:
-      # unicode messes with write()
-      _file.write(message.encode('ascii', 'ignore').decode('utf-8'))
-
-  @staticmethod
-  def get_image(image_url, file='img.png'):
+  def get_image(image_url: str, file: str='img.png') -> str:
+    """Downloads image to set file, returns string"""
     with open(file, 'wb+') as _file:
       _file.write(requests.get(image_url).content)
     return file
@@ -69,7 +95,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
-cbot = Cbot()
+cbot = Bot()
 bot = commands.Bot(command_prefix='>')
 
 # ================ Bot commands ================ #
@@ -92,11 +118,6 @@ async def enhance(ctx, shift: int):
   else:
     _file = cbot.shift_image(ctx.message.attachments[0].url)
   await ctx.send('here you go!', file=_file)
-
-##@bot.command(name='shoo', help='this brutally murders the bot, do not use')
-##async def shoo(ctx):
-##  await ctx.send('bye bye')
-##  exit()
 
 # ================ Bot events ================ #
 @bot.event
